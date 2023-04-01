bb5406f79d47b7c6a7f1b3da43f5afeea7d42958
XW987654 <790423323@qq.com>
2023-04-01 23:31:17 +0800

    [CHG] - change 3.30.1.c

--- 3.30.1.c
+++ 3.30.1.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
+
 int isPrime(int n)//判断素数函数。
 {
     int i, prime = 1;
-    for (i = 2; i < n; ++i){
-        if (n%i == 0){
+
+    for (i = 2; i < n; ++i) {
+        if (n%i == 0) {
             prime = 0;
         }
-    };
-        return prime;
+    }
+    return prime;
 }
+
 int main()
 {
-        int a = 0, b = 0, n = 0, count = 0, sum = 0;
-        int c[200] = {};
-            scanf("%d%d", &a, &b);
-            for(n = 2; count != 200; ){
-                if(isPrime(n)){
-                    c[count] = n;
-                    count++;
-                };
-                n++;}
-                for(;a <= b;++a){
-                    sum += c[a-1];};
-                    printf("%d", sum);
-                    return 0;
+    int a = 0, b = 0;
+    int n = 0;
+    int count = 0;
+    int sum = 0;
+    int c[200] = {};
+
+    scanf("%d%d", &a, &b);
+    for (n = 2; count != 200; ++n) {
+        if(isPrime(n)){
+            c[count] = n;
+            ++count;
+        }
+    }
+    for (;a <= b;++a) {
+        sum += c[a-1];
+    }
+    printf("%d", sum);
+    return 0;
 }
